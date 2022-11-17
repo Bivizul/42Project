b76e97e2875af936cc7ad1de3e864f7fd5aa9199
Bivizul <mamochapa@gmail.com>
2022-11-17 17:18:40 +0400

    Initial commit

---
+++ app/src/main/java/aaa/bivizul/a42project/MainActivity.kt
@@ -0,0 +1,11 @@
+package aaa.bivizul.a42project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
